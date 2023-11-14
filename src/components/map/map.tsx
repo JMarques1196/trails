1abf1dcfb2b0f5e4ed741a8c3286c1151ff13d63
João André Marques <joaoandremarques@gmail.com>
2023-11-14 18:11:06 +0000

    created map + page layout component

--- src/components/map/map.tsx
+++ src/components/map/map.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Map = () => {
-  return <div>Map</div>;
+  return <></>;
 };
 
 export default Map;
--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import Map from "../components/map/map";
 
 const Layout = () => {
-  return <div>Layout</div>;
+  return <Map />;
 };
 
 export default Layout;
