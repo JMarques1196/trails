39d4024b3163cb811334d19dfd18a5a13ecf14e4
João André Marques <joaoandremarques@gmail.com>
2023-11-14 08:50:35 +0000

    added folders to file structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Layout from "./pages/layout";
 
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <>
+      <Layout />
+    </>
+  );
 }
 
 export default App;
