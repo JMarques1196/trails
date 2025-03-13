696219577f969868ab6299b7ac6071764aefa106
João André Marques <joaoandremarques@gmail.com>
2025-03-13 21:07:09 +0000

    feat: add home page

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
-root.render(<App />);
+root.render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
--- vite.config.mts
+++ vite.config.mts
@@ -4,7 +4,7 @@ import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   plugins: [react(), viteTsconfigPaths()],
-  base: "/trails/",
+  base: "/",
   server: {
     open: true,
     port: 3000,
